b9fd5048211d46a903a0c4ca566de8f956e82fcb
hamidu sodiq <hamidusodiq2@gmail.com>
2022-12-30 23:47:33 +0100

    created javascript template for crew profile using json data

---
+++ starter-code/crew.js
@@ -0,0 +1,45 @@
+const crewSection = document.getElementById("crew");
+console.log(crewSection);
+
+let file;
+
+fetch("/starter-code/data.json")
+  .then((response) => response.json())
+  .then((data) => application(data));
+
+const application = function (data) {
+  console.log(data);
+  crewProfile(data, 0);
+};
+
+const crewProfile = function (data, index) {
+  let crewProfileTemplate = `
+    <div class="crew-profile">
+        <div class="crew-image profile-primary">
+            <img src="${data.crew[index].images.webp}" alt="" />
+        </div>
+        <div class="profile-secondary">
+            <div class="crew-slider-button"></div>
+            <div class="crew-description">
+              <h2>${data.crew[index].role}</h2>
+              <h3>${data.crew[index].name}</h3>
+              <p>
+              ${data.crew[index].bio}
+              </p>
+            </div>
+        </div>
+    </div>
+`;
+
+  crewSection.insertAdjacentHTML("beforeend", crewProfileTemplate);
+  console.log(document.querySelector(".crew-slider-button"));
+
+  for (let i = 0; i < data.crew.length; i++) {
+    document
+      .querySelector(".crew-slider-button")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<span class="slide-btn" data-index = ${i}></span>`
+      );
+  }
+};
