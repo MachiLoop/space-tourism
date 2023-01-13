e9e38e23fa6a015623c505319f97ee984a63fb66
hamidu sodiq <hamidusodiq2@gmail.com>
2023-01-13 04:44:00 +0100

    Added js for mobile navigation

---
+++ starter-code/nav.js
@@ -0,0 +1,14 @@
+const navigationEl = document.querySelector("nav");
+const navBtn = document.querySelector(".harmburger");
+
+navBtn.addEventListener("click", function () {
+  let visibility = navigationEl.getAttribute("data-visible");
+
+  if (visibility == "false") {
+    navigationEl.setAttribute("data-visible", "true");
+    navBtn.firstElementChild.src = "assets/shared/icon-close.svg";
+  } else {
+    navigationEl.setAttribute("data-visible", "false");
+    navBtn.firstElementChild.src = "assets/shared/icon-hamburger.svg";
+  }
+});
